f31f8fffc7dd5355f3f727cfee8d5269f8b8429c
Rusty <plilley08@gmail.com>
2022-10-11 22:57:10 -0400

    yes

--- MMR/init.lua
+++ MMR/init.lua
@@ -1,3 +1,11 @@
 repeat task.wait() until game:IsLoaded()
 
-print("Goose")
+local function ensureTycoonLoaded()
+	while true and task.wait() do
+		print("Loading")
+		if game:GetService("Players").LocalPlayer:FindFirstChild("PlayerData", true) then
+			break
+		end
+	end
+end
+ensureTycoonLoaded()
