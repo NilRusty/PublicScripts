bab2a50bb65a072684902daefca7f5c5c96059c2
Rusty <plilley08@gmail.com>
2022-10-11 23:01:56 -0400

    tes

--- MMR/init.lua
+++ MMR/init.lua
@@ -2,10 +2,20 @@ repeat task.wait() until game:IsLoaded()
 
 local function ensureTycoonLoaded()
 	while true and task.wait() do
-		print("Loading")
 		if game:GetService("Players").LocalPlayer:FindFirstChild("PlayerData", true) then
 			break
 		end
 	end
 end
 ensureTycoonLoaded()
+
+
+local Orion = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local MainWindow = Orion:MakeWindow({Name = "Mini Miners Menu", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local Tab1 = Window:MakeTab({
+	Name = "Miners Haven",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
